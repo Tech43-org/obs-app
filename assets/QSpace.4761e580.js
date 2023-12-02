dd25344e2de827eeadcef62bb58caee5bff76022
Martin R <no-reply@tech43.org>
2023-12-02 20:30:14 +0700

    🚀 2023-12-02 20:30:14

---
+++ assets/QSpace.4761e580.js
@@ -0,0 +1,1 @@
+import{A as a,s as e}from"./index.efca5f7a.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ assets/pinia.6f9eaf82.js
@@ -0,0 +1,1 @@
+import{b as c,c as S,u,a as f,d,e as l,f as p}from"./index.efca5f7a.js";console.log("Initialize Boot Section");console.log("  \u221F pinia.ts");var m=c(({app:t})=>{const o=S();t.use(o);const s=u(),n=f(),i=d(),r=l(),a=p(),e=window;typeof window!="undefined"&&(e.settingsStore=s,e.userStore=n,e.menuStore=i,e.activityStore=r,e.financeStore=a)});export{m as default};
