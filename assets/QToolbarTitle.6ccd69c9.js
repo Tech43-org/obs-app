c3a961539ef841be44158389f4aa7d4e63d8f341
Martin R <no-reply@tech43.org>
2023-12-02 12:28:38 +0700

    🚀 2023-12-02 12:28:38

---
+++ assets/QToolbarTitle.6ccd69c9.js
@@ -0,0 +1,1 @@
+import{s as a,z as t,A as l,B as r}from"./index.943c308d.js";var n=a({name:"QToolbarTitle",props:{shrink:Boolean},setup(s,{slots:o}){const e=t(()=>"q-toolbar__title ellipsis"+(s.shrink===!0?" col-shrink":""));return()=>l("div",{class:e.value},r(o.default))}});export{n as Q};
