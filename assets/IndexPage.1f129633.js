d3470975d2ca336081251c618a7325d483beeaad
Martin R <no-reply@tech43.org>
2023-12-19 14:21:36 +0700

    🚀 2023-12-19 14:21:36

---
+++ assets/IndexPage.1f129633.js
@@ -0,0 +1,1 @@
+import{Q as o}from"./QPage.eee2d5a7.js";import{k as s,Z as t,$ as r,o as n,m as a}from"./index.1bbbef81.js";const f=s({__name:"IndexPage",setup(c){const e=t();return r(()=>{e.push("/settings")}),(u,m)=>(n(),a(o,{class:"row items-center justify-evenly"}))}});export{f as default};
