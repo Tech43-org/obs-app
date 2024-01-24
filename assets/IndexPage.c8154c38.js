8068b2bd6439bf2901d1f42591a24703873433d4
Martin R <no-reply@tech43.org>
2024-01-24 13:09:51 +0700

    🚀 2024-01-24 13:09:51

---
+++ assets/IndexPage.c8154c38.js
@@ -0,0 +1,1 @@
+import{Q as o}from"./QPage.b48c2029.js";import{i as s,Z as t,$ as r,o as n,k as a}from"./index.47b229d6.js";const _=s({__name:"IndexPage",setup(c){const e=t();return r(()=>{e.push("/settings")}),(u,p)=>(n(),a(o,{class:"row items-center justify-evenly"}))}});export{_ as default};
---
+++ assets/QSpace.cf08d7d1.js
@@ -0,0 +1,1 @@
+import{A as a,s as e}from"./index.47b229d6.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
